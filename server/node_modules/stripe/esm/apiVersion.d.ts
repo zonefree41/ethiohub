3cbe2da7cdbd1799a8f573284c30af79f2b6d371
Luel Degnaw <zonefree41@gmail.com>
2026-08-21 23:42:23 -0400

    Stop tracking server node_modules

--- server/node_modules/stripe/esm/apiVersion.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const ApiVersion = "2026-04-22.dahlia";
-export declare const ApiMajorVersion = "dahlia";
--- server/node_modules/stripe/esm/apiVersion.js
+++
@@ -1,4 +0,0 @@
-// File generated from our OpenAPI spec
-export const ApiVersion = '2026-04-22.dahlia';
-export const ApiMajorVersion = 'dahlia';
-//# sourceMappingURL=apiVersion.js.map
